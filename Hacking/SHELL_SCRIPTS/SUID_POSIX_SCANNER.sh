e09ec9e85ef80e038b6c4b99fd2363deb0fd203f
ChrisC0O <136499908+ChrisC0O@users.noreply.github.com>
2026-02-23 12:54:06 +0100

    Update SUID_POSIX_SCANNER.sh

--- Hacking/SHELL_SCRIPTS/SUID_POSIX_SCANNER.sh
+++ Hacking/SHELL_SCRIPTS/SUID_POSIX_SCANNER.sh
@@ -1,16 +1,7 @@
 find / -perm -4000 -type f 2>/dev/null | while read f; do
-    exe="NO"
-    write="NO"
-    caps="$(getcap "$f" 2>/dev/null)"
-
-    [ -x "$f" ] && exe="YES"
-    [ -w "$f" ] && write="YES !!"
-
-    [ -z "$caps" ] && caps="none"
-
-    printf "\n%s\n" "$f"
-    printf "  Executable : %s\n" "$exe"
-    printf "  Writable   : %s\n" "$write"
-    printf "  Capabilities: %s\n" "$caps"
-    printf '%s\n' "------------------------------------------------------------"
+    printf "\n[SUID] %s\n" "$f"
+    printf "  Run        : %s\n" "$( [ -x "$f" ] && echo YES || echo NO )"
+    printf "  Writable   : %s\n" "$( [ -w "$f" ] && echo YES || echo NO )"
+    printf "  POSIX Cap  : %s\n" "$(getcap "$f" 2>/dev/null || echo none)"
+    printf '%s\n' "----------------------------------------"
 done
