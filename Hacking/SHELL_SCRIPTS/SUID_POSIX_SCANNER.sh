0516476e13d9d23c3ba6a66478751b630c87e3c7
ChrisC0O <136499908+ChrisC0O@users.noreply.github.com>
2026-02-23 12:53:04 +0100

    Update SUID_POSIX_SCANNER.sh

--- Hacking/SHELL_SCRIPTS/SUID_POSIX_SCANNER.sh
+++ Hacking/SHELL_SCRIPTS/SUID_POSIX_SCANNER.sh
@@ -1,5 +1,16 @@
-find / -perm -4000 -type f 2>/dev/null -exec sh -c '
-for f; do
-  [ -x "$f" ] && printf "%-50s | %s\n" "$f" "$(getcap "$f" 2>/dev/null)"
+find / -perm -4000 -type f 2>/dev/null | while read f; do
+    exe="NO"
+    write="NO"
+    caps="$(getcap "$f" 2>/dev/null)"
+
+    [ -x "$f" ] && exe="YES"
+    [ -w "$f" ] && write="YES !!"
+
+    [ -z "$caps" ] && caps="none"
+
+    printf "\n%s\n" "$f"
+    printf "  Executable : %s\n" "$exe"
+    printf "  Writable   : %s\n" "$write"
+    printf "  Capabilities: %s\n" "$caps"
+    printf '%s\n' "------------------------------------------------------------"
 done
-' sh {} +
